411ba0806d8012cfaf5bcbf3fce20870eccc93f6
Cristhian Paucara <ivan.paucara@uab.edu.bo>
2023-03-28 22:50:49 -0400

    diseño_bajo_template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LayoutwebsiteComponent } from './layouts/layoutwebsite/layoutwebsite.component';
+const routes: Routes = [
+  {path:'', redirectTo:'website', pathMatch:'full'},//para redireccionar a website cuando hay errores
+  {path:'', 
+  component: LayoutwebsiteComponent,
+  children:[
+            { path: 'website', loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule) }
+          ]
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
